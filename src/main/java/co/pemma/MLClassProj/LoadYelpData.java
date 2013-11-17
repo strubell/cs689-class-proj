bc706e6dc36a289e8577fe3b5546689df6e54041
pv <pat@cs.umass.edu>
2013-11-17 14:44:45 -0500

    min java importer

--- src/main/java/co/pemma/MLClassProj/LoadYelpData.java
+++ src/main/java/co/pemma/MLClassProj/LoadYelpData.java
@@ -1,8 +1,32 @@
 package co.pemma.MLClassProj;
 
-public class LoadYelpData{
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public static void main(String[] args){
-        System.out.println("hey buttholer");
-    }   
+import com.eclipsesource.json.JsonObject;
+
+
+public class LoadYelpData {
+
+	public static void main(String[] args) throws IOException {
+		String file =  "yelp_phoenix_academic_dataset/yelp_academic_dataset_review.json" ;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));	
+
+			String line ="";
+			while ((line = reader.readLine()) != null)
+			{				
+				JsonObject jsonObject = JsonObject.readFrom(line);
+				System.out.println(jsonObject.get( "stars" ) + " " + jsonObject.get( "text" ).asString());
+			}
+
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
