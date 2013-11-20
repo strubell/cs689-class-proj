7c2e32df268679480e4a2d529bc62ae0ea2ffa81
pv <pat@cs.umass.edu>
2013-11-20 00:49:21 -0500

    new features, might work

--- src/main/java/co/pemma/MLClassProj/GetSyntacticFeatures.java
+++ src/main/java/co/pemma/MLClassProj/GetSyntacticFeatures.java
@@ -31,11 +31,23 @@ public class GetSyntacticFeatures {
 	private static final int WORD_IDX = 2;
 	private static final int POS_IDX = 3;
 	private static final int DEP_IDX = 5;
+	
+	// vector indices
+	private static final int SYNTAX_FEATURE_COUNT = 95;
+	private static final int WORD_SHAPE = 0;
+	private static final int WORD_COUNT = 5;
+	private static final int LETTERS = 7;
+	private static final int DIGITS = 33;
+	private static final int WORD_LENGTH_FREQ = 43;
+	private static final int PUNCTUATION = 63;
+	private static final int SPEACIAL_CHARS = 74;
+
+
 
 	private static final int BIG_ARR_SIZE = 1000;
 
 	private static final String OUTPUT_DIR = "output";
-	
+
 	private static final int USER_REVIEW_THRESHOLD = 250;
 
 	private static final String[] FUNCTION_TAGS = { 
@@ -79,13 +91,156 @@ public class GetSyntacticFeatures {
 		"possessive", "preconj", "predet", "prep", "prt", "punct",
 		"quantmod", "rcmod", "root", "xcomp" };
 
+
+	private static double[] syntaxVector = new double[SYNTAX_FEATURE_COUNT];
+
+	private static double[] syntaxFeatures(String word)
+	{
+		boolean firstUpper = false;
+		boolean otherUpper = false;
+		boolean allUpper = true;
+		int wordShape = 0;
+
+		// word count
+		syntaxVector[WORD_COUNT] ++;		
+		// character count
+		syntaxVector[WORD_COUNT + 1] += word.length();
+		
+		// word length frequency (lengths 1-20)
+		syntaxVector[WORD_LENGTH_FREQ + Math.max(word.length(), 20) - 1] += 1;
+
+		
+		for (int i = 0; i < word.length(); i ++)
+		{
+			char c = word.charAt(i);
+			
+			// digits occurance
+			if (c >= 0 && c <= 9 )
+				syntaxVector[DIGITS + c] ++;
+			
+			// punctuation
+			else if (c == '.')
+				syntaxVector[PUNCTUATION] ++;
+			else if (c == '?')
+				syntaxVector[PUNCTUATION + 1] ++;
+			else if (c == '!')
+				syntaxVector[PUNCTUATION + 2] ++;
+			else if (c == ',')
+				syntaxVector[PUNCTUATION + 3] ++;
+			else if (c == ';')
+				syntaxVector[PUNCTUATION + 4] ++;
+			else if (c == ':')
+				syntaxVector[PUNCTUATION + 5] ++;
+			else if (c == '(')
+				syntaxVector[PUNCTUATION + 6] ++;
+			else if (c == ')')
+				syntaxVector[PUNCTUATION + 7] ++;
+			else if (c == '\"')
+				syntaxVector[PUNCTUATION + 8] ++;
+			else if (c == '-')
+				syntaxVector[PUNCTUATION + 9] ++;
+			else if (c == '\'')
+				syntaxVector[PUNCTUATION + 10] ++;
+
+			// special chars
+			else if (c == '`')
+				syntaxVector[SPEACIAL_CHARS] ++;
+			else if (c == '~')
+				syntaxVector[SPEACIAL_CHARS + 1] ++;
+			else if (c == '@')
+				syntaxVector[SPEACIAL_CHARS + 2] ++;
+			else if (c == '#')
+				syntaxVector[SPEACIAL_CHARS + 3] ++;
+			else if (c == '$')
+				syntaxVector[SPEACIAL_CHARS + 4] ++;
+			else if (c == '%')
+				syntaxVector[SPEACIAL_CHARS + 5] ++;
+			else if (c == '^')
+				syntaxVector[SPEACIAL_CHARS + 6] ++;
+			else if (c == '&')
+				syntaxVector[SPEACIAL_CHARS + 7] ++;
+			else if (c == '*')
+				syntaxVector[SPEACIAL_CHARS + 8] ++;
+			else if (c == '_')
+				syntaxVector[SPEACIAL_CHARS + 9] ++;
+			else if (c == '+')
+				syntaxVector[SPEACIAL_CHARS + 10] ++;
+			else if (c == '=')
+				syntaxVector[SPEACIAL_CHARS + 11] ++;
+			else if (c == '[')
+				syntaxVector[SPEACIAL_CHARS + 12] ++;
+			else if (c == ']')
+				syntaxVector[SPEACIAL_CHARS + 13] ++;
+			else if (c == '{')
+				syntaxVector[SPEACIAL_CHARS + 14] ++;
+			else if (c == '}')
+				syntaxVector[SPEACIAL_CHARS + 15] ++;
+			else if (c == '\\')
+				syntaxVector[SPEACIAL_CHARS + 16] ++;
+			else if (c == '|')
+				syntaxVector[SPEACIAL_CHARS + 17] ++;
+			else if (c == '/')
+				syntaxVector[SPEACIAL_CHARS + 18] ++;
+			else if (c == '<')
+				syntaxVector[SPEACIAL_CHARS + 19] ++;
+			else if (c == '>')
+				syntaxVector[SPEACIAL_CHARS + 20] ++;
+
+			// letter occurance
+			else if (c >= 'a' && c <= 'z')
+				syntaxVector[LETTERS + (c - 'a')] ++;
+			else if (c >= 'A' && c <= 'Z')
+			{
+				syntaxVector[LETTERS + (c - 'A')] ++;
+				
+				// word shape
+				if (i == 0)
+					firstUpper = true;
+				else if (firstUpper == false && otherUpper == true) 
+					wordShape = 4; // other
+
+				else if (firstUpper == false && otherUpper == false)
+				{
+					wordShape = 3; // camelCap
+					otherUpper = true;
+				}
+			}
+			else allUpper = false;
+
+		}
+		// word shape 
+		if (allUpper) wordShape = 0; // all upper
+		else if (!firstUpper && !otherUpper) wordShape = 1; // all lower
+		else if (firstUpper && !otherUpper) wordShape = 2; // only first upper
+		syntaxVector[WORD_SHAPE + wordShape] += 1;
+		
+		return null;
+	}
+
+	//
+	//	// Yules K, frequency of once used, twice used etc words [11]
+	//	private double[] vocabularyRichness(String word)
+	//	{
+	//
+	//	}
+	//
+
+
+	//	/*  frequency of syntax pairs 		[789]
+	//	/	(A,B) where A is parent of B
+	//	 */
+	//	private double[] syntaxCategoryPairs(String word)
+	//	{
+	//
+	//	}
+
 	public static void main(String[] args) {
 
 		Configuration config = new Configuration();
 		Path path = new Path(OUTPUT_DIR + File.separator + "features");
 		try(FileSystem fs = FileSystem.get(config);
-			SequenceFile.Writer mahoutWriter = new SequenceFile.Writer(fs, config, path, Text.class, VectorWritable.class)){
-			
+				SequenceFile.Writer mahoutWriter = new SequenceFile.Writer(fs, config, path, Text.class, VectorWritable.class)){
+
 			VectorWritable vec = new VectorWritable();
 
 			long t0 = System.currentTimeMillis();
@@ -143,6 +298,9 @@ public class GetSyntacticFeatures {
 									String pos = splitLine[POS_IDX];
 									String dep = splitLine[DEP_IDX];
 
+									// extract other features
+									syntaxFeatures(word);
+									
 									posTotals.put(pos, posTotals.get(pos) + 1.0);
 									parseTotals.put(dep, parseTotals.get(dep) + 1.0);
 
@@ -168,7 +326,7 @@ public class GetSyntacticFeatures {
 
 			int numObservedFuncWords = functionTotals.keySet().size();
 			reviewCount = 1;
-			
+
 			// now gather individual document counts and write to file
 			for (User user : userList.subList(startIndex, startIndex + numToTake)) {
 				for (Review review : user.reviews) {
@@ -193,13 +351,13 @@ public class GetSyntacticFeatures {
 						Map<String, Double> posPerDoc = Util.newMapFromKeySet(posTags, 0.0);
 						Map<String, Double> parsePerDoc = Util.newMapFromKeySet(parseLabels, 0.0);
 						Map<String, Double> functionPerDoc = Util.newMapFromKeySet(functionTotals.keySet(), 0.0);
-						
+
 						while ((line = reader.readLine()) != null) {
 							if (!line.equals("")) {
 								//System.out.println(line);
 								String[] splitLine = line.split("\\s+");
 								if (splitLine.length == 6) {
-									
+
 									String word = splitLine[WORD_IDX];
 									String pos = splitLine[POS_IDX];
 									String dep = splitLine[DEP_IDX];
@@ -208,7 +366,7 @@ public class GetSyntacticFeatures {
 									parsePerDoc.put(dep, parsePerDoc.get(dep) + 1.0);
 
 									String lowerWord = word.toLowerCase();
-									
+
 									if (functionPosTags.contains(pos))
 										functionPerDoc.put(lowerWord, functionPerDoc.get(lowerWord) + 1.0);
 								}
@@ -231,7 +389,7 @@ public class GetSyntacticFeatures {
 							allFreqs[i] = freq;
 							i++;
 						}
-						
+
 						int j = 0;
 						for(Entry<String,Double> entry : parsePerDoc.entrySet()){
 							String key = entry.getKey();
@@ -242,7 +400,7 @@ public class GetSyntacticFeatures {
 							allFreqs[i+j] = freq;
 							j++;
 						}
-						
+
 						int k = 0;
 						for(Entry<String,Double> entry : functionPerDoc.entrySet()){
 							String key = entry.getKey();
@@ -259,14 +417,14 @@ public class GetSyntacticFeatures {
 						NamedVector featureVector = new NamedVector(new DenseVector(allFreqs), vectorKey);
 						vec.set(featureVector);
 						mahoutWriter.append(new Text(vectorKey), vec);
-						
+
 						if(reviewCount % 100 == 0){
 							for(int m = 0; m < allFreqs.length; ++m){
 								System.out.print(allFreqs[m] + " ");
 							}
 							System.out.println();
 						}
-						
+
 						reviewCount++;
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
@@ -279,4 +437,11 @@ public class GetSyntacticFeatures {
 			e.printStackTrace();
 		}
 	}
+
 }
+
+
+
+
+
+
